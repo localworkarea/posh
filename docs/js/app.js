9b9468694603b06b862cc2c3b9806a2a3a408bcc
Denys Mykhailenko <localworkarea@gmail.com>
2024-01-29 12:32:26 +0200

    upda

--- docs/js/app.js
+++ docs/js/app.js
@@ -20,6 +20,52 @@
             }), 0);
         }));
     }
+    let bodyLockStatus = true;
+    let bodyLockToggle = (delay = 500) => {
+        if (document.documentElement.classList.contains("lock")) bodyUnlock(delay); else bodyLock(delay);
+    };
+    let bodyUnlock = (delay = 500) => {
+        let body = document.querySelector("body");
+        if (bodyLockStatus) {
+            let lock_padding = document.querySelectorAll("[data-lp]");
+            setTimeout((() => {
+                for (let index = 0; index < lock_padding.length; index++) {
+                    const el = lock_padding[index];
+                    el.style.paddingRight = "0px";
+                }
+                body.style.paddingRight = "0px";
+                document.documentElement.classList.remove("lock");
+            }), delay);
+            bodyLockStatus = false;
+            setTimeout((function() {
+                bodyLockStatus = true;
+            }), delay);
+        }
+    };
+    let bodyLock = (delay = 500) => {
+        let body = document.querySelector("body");
+        if (bodyLockStatus) {
+            let lock_padding = document.querySelectorAll("[data-lp]");
+            for (let index = 0; index < lock_padding.length; index++) {
+                const el = lock_padding[index];
+                el.style.paddingRight = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
+            }
+            body.style.paddingRight = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
+            document.documentElement.classList.add("lock");
+            bodyLockStatus = false;
+            setTimeout((function() {
+                bodyLockStatus = true;
+            }), delay);
+        }
+    };
+    function menuInit() {
+        if (document.querySelector(".icon-menu")) document.addEventListener("click", (function(e) {
+            if (bodyLockStatus && e.target.closest(".icon-menu")) {
+                bodyLockToggle();
+                document.documentElement.classList.toggle("menu-open");
+            }
+        }));
+    }
     let addWindowScrollEvent = false;
     function headerScroll() {
         addWindowScrollEvent = true;
@@ -55,23 +101,150 @@
             }));
         }
     }), 0);
-    document.addEventListener("DOMContentLoaded", (function() {
-        const links = document.querySelectorAll(".link-page");
-        links.forEach((function(link) {
-            link.addEventListener("click", (function(event) {
-                document.documentElement.classList.add("preloader");
-                event.preventDefault();
-                const href = this.getAttribute("href");
-                const mask = document.querySelector(".mask");
-                if (mask) mask.classList.add("active");
-                setTimeout((function() {
-                    window.location.href = href;
-                }), 600);
+    class DynamicAdapt {
+        constructor(type) {
+            this.type = type;
+        }
+        init() {
+            this.оbjects = [];
+            this.daClassname = "_dynamic_adapt_";
+            this.nodes = [ ...document.querySelectorAll("[data-da]") ];
+            this.nodes.forEach((node => {
+                const data = node.dataset.da.trim();
+                const dataArray = data.split(",");
+                const оbject = {};
+                оbject.element = node;
+                оbject.parent = node.parentNode;
+                оbject.destination = document.querySelector(`${dataArray[0].trim()}`);
+                оbject.breakpoint = dataArray[1] ? dataArray[1].trim() : "767";
+                оbject.place = dataArray[2] ? dataArray[2].trim() : "last";
+                оbject.index = this.indexInParent(оbject.parent, оbject.element);
+                this.оbjects.push(оbject);
             }));
+            this.arraySort(this.оbjects);
+            this.mediaQueries = this.оbjects.map((({breakpoint}) => `(${this.type}-width: ${breakpoint}px),${breakpoint}`)).filter(((item, index, self) => self.indexOf(item) === index));
+            this.mediaQueries.forEach((media => {
+                const mediaSplit = media.split(",");
+                const matchMedia = window.matchMedia(mediaSplit[0]);
+                const mediaBreakpoint = mediaSplit[1];
+                const оbjectsFilter = this.оbjects.filter((({breakpoint}) => breakpoint === mediaBreakpoint));
+                matchMedia.addEventListener("change", (() => {
+                    this.mediaHandler(matchMedia, оbjectsFilter);
+                }));
+                this.mediaHandler(matchMedia, оbjectsFilter);
+            }));
+        }
+        mediaHandler(matchMedia, оbjects) {
+            if (matchMedia.matches) оbjects.forEach((оbject => {
+                this.moveTo(оbject.place, оbject.element, оbject.destination);
+            })); else оbjects.forEach((({parent, element, index}) => {
+                if (element.classList.contains(this.daClassname)) this.moveBack(parent, element, index);
+            }));
+        }
+        moveTo(place, element, destination) {
+            element.classList.add(this.daClassname);
+            if (place === "last" || place >= destination.children.length) {
+                destination.append(element);
+                return;
+            }
+            if (place === "first") {
+                destination.prepend(element);
+                return;
+            }
+            destination.children[place].before(element);
+        }
+        moveBack(parent, element, index) {
+            element.classList.remove(this.daClassname);
+            if (parent.children[index] !== void 0) parent.children[index].before(element); else parent.append(element);
+        }
+        indexInParent(parent, element) {
+            return [ ...parent.children ].indexOf(element);
+        }
+        arraySort(arr) {
+            if (this.type === "min") arr.sort(((a, b) => {
+                if (a.breakpoint === b.breakpoint) {
+                    if (a.place === b.place) return 0;
+                    if (a.place === "first" || b.place === "last") return -1;
+                    if (a.place === "last" || b.place === "first") return 1;
+                    return 0;
+                }
+                return a.breakpoint - b.breakpoint;
+            })); else {
+                arr.sort(((a, b) => {
+                    if (a.breakpoint === b.breakpoint) {
+                        if (a.place === b.place) return 0;
+                        if (a.place === "first" || b.place === "last") return 1;
+                        if (a.place === "last" || b.place === "first") return -1;
+                        return 0;
+                    }
+                    return b.breakpoint - a.breakpoint;
+                }));
+                return;
+            }
+        }
+    }
+    const da = new DynamicAdapt("max");
+    da.init();
+    document.addEventListener("DOMContentLoaded", (function() {
+        window.addEventListener("load", (function() {
+            var video = document.getElementById("heroVideo");
+            if (video) {
+                var source = video.querySelector("source");
+                var videoLoaded = localStorage.getItem("videoLoaded");
+                video.addEventListener("canplay", (function() {
+                    video.play();
+                }));
+                if (!videoLoaded) setTimeout((function() {
+                    video.load();
+                    source.src = source.getAttribute("data-src");
+                    video.load();
+                    localStorage.setItem("videoLoaded", "true");
+                }), 2e3); else {
+                    video.load();
+                    source.src = source.getAttribute("data-src");
+                    video.load();
+                }
+            }
+        }));
+        var video = document.getElementById("heroVideo");
+        var playPauseButton = document.querySelector(".hero__control");
+        if (playPauseButton) playPauseButton.addEventListener("click", (function() {
+            if (video.paused) {
+                video.play();
+                playPauseButton.classList.remove("paused");
+            } else {
+                video.pause();
+                playPauseButton.classList.add("paused");
+            }
+        }));
+        var btnMaskElements = document.querySelectorAll(".btn-mask");
+        if (btnMaskElements.length > 0) btnMaskElements.forEach((function(btnMaskElement) {
+            var clone = btnMaskElement.querySelector(".btn-mask__body").cloneNode(true);
+            clone.classList.add("btn-mask-clone");
+            btnMaskElement.appendChild(clone);
         }));
     }));
+    const header = document.querySelector("header");
+    const heroBg = document.querySelector(".hero__bg");
+    const heroBody = document.querySelector(".hero__body");
+    const heroControlButton = document.querySelector(".hero__control");
+    if (heroBg) {
+        const addHoverClass = () => {
+            if (window.innerWidth > 30.06125 * parseFloat(getComputedStyle(document.documentElement).fontSize)) heroBg.classList.add("hover");
+        };
+        const removeHoverClass = () => {
+            if (window.innerWidth > 30.06125 * parseFloat(getComputedStyle(document.documentElement).fontSize)) heroBg.classList.remove("hover");
+        };
+        header.addEventListener("mouseover", addHoverClass);
+        heroBody.addEventListener("mouseover", addHoverClass);
+        heroControlButton.addEventListener("mouseover", addHoverClass);
+        header.addEventListener("mouseout", removeHoverClass);
+        heroBody.addEventListener("mouseout", removeHoverClass);
+        heroControlButton.addEventListener("mouseout", removeHoverClass);
+    }
     window["FLS"] = false;
     isWebp();
     addLoadedClass();
+    menuInit();
     headerScroll();
 })();
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,26 +3,123 @@ import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
 
-document.addEventListener('DOMContentLoaded', function () {
-  const links = document.querySelectorAll('.link-page');
+// document.addEventListener('DOMContentLoaded', function () {
+//   const links = document.querySelectorAll('.link-page');
 
-  links.forEach(function (link) {
-      link.addEventListener('click', function (event) {
-          // Добавляем класс preloader
-          document.documentElement.classList.add('preloader');
+//   links.forEach(function (link) {
+//       link.addEventListener('click', function (event) {
+//           // Добавляем класс preloader
+//           document.documentElement.classList.add('preloader');
 
-          // Предотвращаем сразу переход по ссылке
-          event.preventDefault();
+//           // Предотвращаем сразу переход по ссылке
+//           event.preventDefault();
 
-          const href = this.getAttribute('href');
-          const mask = document.querySelector('.mask');
-          if (mask) {
-              mask.classList.add('active');
-          }
+//           const href = this.getAttribute('href');
+//           const mask = document.querySelector('.mask');
+//           if (mask) {
+//               mask.classList.add('active');
+//           }
           
-          setTimeout(function () {
-              window.location.href = href;
-          }, 600);
-      });
-  });
+//           setTimeout(function () {
+//               window.location.href = href;
+//           }, 600);
+//       });
+//   });
+// });
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    // ОТЛОЖЕННАЯ ЗАГРУЗКА ВИДЕО ========================================================
+        window.addEventListener("load", function() {
+            var video = document.getElementById("heroVideo");
+            if (video) {
+                var source = video.querySelector("source");
+                // Проверка локального хранилища на наличие ключа
+                var videoLoaded = localStorage.getItem("videoLoaded");
+                video.addEventListener("canplay", function() {
+                    video.play();
+                });
+                // Если видео не было загружено ранее, устанавливаем таймер на 2 секунды
+                if (!videoLoaded) {
+                    setTimeout(function() {
+                        video.load();
+                        source.src = source.getAttribute("data-src");
+                        video.load();
+                        // Сохраняем информацию о загрузке видео в локальное хранилище
+                        localStorage.setItem("videoLoaded", "true");
+                    }, 2000);
+                } else {
+                    // Если видео уже было загружено, начинаем его воспроизведение сразу
+                    video.load();
+                    source.src = source.getAttribute("data-src");
+                    video.load();
+                }
+            }
+        });
+    // -------------------------------------------------------------------------------------
+
+
+    // ОСТАНОВИТЬ/ВОСПРОИЗВЕСТИ ГЛАВНОЕ ВИДЕО ПО КЛИКУ ===========================================
+        var video = document.getElementById('heroVideo');
+        var playPauseButton = document.querySelector('.hero__control');
+
+        if (playPauseButton) {
+            playPauseButton.addEventListener('click', function () {
+                if (video.paused) {
+                    video.play();
+                    playPauseButton.classList.remove('paused');
+                } else {
+                    video.pause();
+                    playPauseButton.classList.add('paused');
+                }
+            });
+        }
+    // -------------------------------------------------------------------------------------
+
+
+    // КЛОНИРОВАНИЕ КНОПОК .btn-mask ==============================================================
+        // Находим все элементы с классом btn-mask
+        var btnMaskElements = document.querySelectorAll('.btn-mask');
+        // Проверяем, что есть хотя бы один элемент с классом btn-mask
+        if (btnMaskElements.length > 0) {
+            // Проходимся по каждому элементу
+            btnMaskElements.forEach(function(btnMaskElement) {
+                // Создаем клон внутреннего элемента btn-mask__body
+                var clone = btnMaskElement.querySelector('.btn-mask__body').cloneNode(true);
+                // Добавляем класс btn-mask-clone к клону
+                clone.classList.add('btn-mask-clone');
+                // Добавляем клон под оригинальным элементом
+                btnMaskElement.appendChild(clone);
+            });
+        }
+    // -------------------------------------------------------------------------------------
+
 });
+
+
+const header = document.querySelector('header');
+const heroBg = document.querySelector('.hero__bg');
+const heroBody = document.querySelector('.hero__body');
+const heroControlButton = document.querySelector('.hero__control');
+
+if (heroBg) {
+    const addHoverClass = () => {
+        if (window.innerWidth > 30.06125 * parseFloat(getComputedStyle(document.documentElement).fontSize)) {
+            heroBg.classList.add('hover');
+        }
+    };
+
+    const removeHoverClass = () => {
+        if (window.innerWidth > 30.06125 * parseFloat(getComputedStyle(document.documentElement).fontSize)) {
+            heroBg.classList.remove('hover');
+        }
+    };
+
+    header.addEventListener('mouseover', addHoverClass);
+    heroBody.addEventListener('mouseover', addHoverClass);
+    heroControlButton.addEventListener('mouseover', addHoverClass);
+
+    header.addEventListener('mouseout', removeHoverClass);
+    heroBody.addEventListener('mouseout', removeHoverClass);
+    heroControlButton.addEventListener('mouseout', removeHoverClass);
+}
