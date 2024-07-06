ed9dba56a130f1417b3fd6e1db38681febeb4a2b
localworkarea <localworkarea@gmail.com>
2024-07-06 15:07:39 +0300

    up

--- src/files/gsap/animApproach.js
+++ src/files/gsap/animApproach.js
@@ -1,23 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-  if (document.documentElement.requestFullscreen) {
-      // document.documentElement.requestFullscreen();
-  } else if (document.documentElement.mozRequestFullScreen) { /* Firefox */
-      document.documentElement.mozRequestFullScreen();
-  } else if (document.documentElement.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-      document.documentElement.webkitRequestFullscreen();
-  } else if (document.documentElement.msRequestFullscreen) { /* IE/Edge */
-      document.documentElement.msRequestFullscreen();
-  }
-});
 
 
 document.addEventListener("DOMContentLoaded", function() {
 
   if (typeof gsap !== 'undefined') {
-
-
-
-
 
     gsap.registerPlugin(ScrollTrigger,ScrollToPlugin)
 
